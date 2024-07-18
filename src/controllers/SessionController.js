3b37aa68e61660a3e0fd815ac440763ab3005eee
dededevelop <andre.admft@gmail.com>
2024-07-18 00:29:11 -0300

    adicionando verifição pela biblioteca YUP

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -9,11 +9,22 @@ destroy: quando queremos deletar uma sessao
 */ 
 
 import User from '../models/User';
+import * as YUP from 'yup';
 
 class SessionController{
 
   async store(req, res){
+    const schema = YUP.object().shape({
+      email: YUP.string().email().required(),
+
+    });
+
     const { email } = req.body;
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({ error: 'Falha na validação.'});
+
+    }
 
     //Verificando se esse usuario já existe
     let user = await User.findOne({ email });
